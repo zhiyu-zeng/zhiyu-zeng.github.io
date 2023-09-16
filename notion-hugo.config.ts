c856ce1a1cf3941cc35bce8074dd64a619e8bc7d
zhiyu <208557110@qq.com>
2023-09-16 08:55:43 +0800

    修改 notion-hugo.config.ts 中的 notion page_url 指向链接

--- notion-hugo.config.ts
+++ notion-hugo.config.ts
@@ -3,7 +3,7 @@ import { UserConfig } from "./src/config"
 const userConfig: UserConfig = {
     mount: {
         manual: false,
-        page_url: 'https://pcloud.notion.site/Notion-DoIt-04bcc51cfe4c49938229c35e4f0a6fb6',
+        page_url: 'https://www.notion.so/Notion-DoIt-67a4546fe2b641c1800206823b2b212e',
         pages: [
             // {
             //     page_id: '<page_id>',
